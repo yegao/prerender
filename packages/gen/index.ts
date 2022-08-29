2d7e5d1a93ecadf714bb4f0788d699a198fd874d
yegao <familyink@163.com>
2022-08-29 17:15:18 +0800

    修改

--- packages/gen/index.ts
+++ packages/gen/index.ts
@@ -5,20 +5,17 @@ import fs from 'fs';
 
 const parsed = parseArgv(process.argv);
 (async function () {
-    console.log(__dirname, process.cwd());
-    console.log(parsed);
     if (parsed === null) {
         process.exitCode = 2;
         return;
     }
     const { content, opts } = parsed;
-    console.log(content, opts);
     const config = JSON.parse(content);
     const staticPaths = Object.keys(config);
     for (const staticPath of staticPaths) {
         const uri = config[staticPath];
         const staticContent = await generate(uri);
-        console.log(staticPath);
+        console.log(uri + '->' + staticPath);
         const absoluteStaticPath = path.resolve(opts.dir, staticPath);
         const chunks = absoluteStaticPath.split('/');
         const filename = chunks.pop();
